efcfdf3c72001a6c9c7e4bf24e8a53c17a2dc905
Christoph Hofer <nubch2@gmail.com>
2022-10-29 14:34:09 +0200

    Update app.js

--- Lab2/webapp/app.js
+++ Lab2/webapp/app.js
@@ -3,13 +3,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
   
-app.listen(PORT, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running")
-    else 
-        console.log("Error occurred, server can't start", error);
-    }
-);
 
 app.get('/', (req, res)=>{
     res.status(200);
